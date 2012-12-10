2f3df26a1bfe64505ac54f97cc98ad6bf6d33141
Oleg Bulatov <oleg@bulatov.me>
2012-12-11 00:16:54 +0400

    Initial commit
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

---
+++ julien.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var Scheduler = require('./scheduler');
+
+x = new Scheduler([
+    {
+        run: function(cb) {
+            console.log("Hello, world!");
+            cb([]);
+        },
+        delay: 1000
+    }
+]);
+x.run();
---
+++ scheduler.js
@@ -0,0 +1,79 @@
+exports = module.exports = function Scheduler(tasks) {
+    this._queue = [];
+    this._tasks = tasks;
+}
+
+exports.prototype.run = function run() {
+    this.firstRun();
+    this.processQueue();
+}
+
+exports.prototype.busyWaitDelay = 1000;
+
+exports.prototype.firstRunDelay = 100;
+
+exports.prototype.dispatch = function dispatch(task, callback) {
+    var _queue = this._queue;
+
+    task.run(function(result) {
+        console.log(result);
+
+        var now = new Date();
+        task._runAt += task.delay;
+        if (task._runAt < now) {
+            console.log('task too slow', task);
+
+            var n = Math.floor((now - task._runAt)/task.delay);
+            task._runAt += (n + 1)*task.delay;
+        }
+
+        for (var i = _queue.length - 1; i >= -1; --i) {
+            if (i == -1 || task._runAt >= _queue[i]._runAt) {
+                _queue.splice(i + 1, 0, task);
+                break;
+            }
+        }
+
+        if (callback) {
+            callback();
+        }
+    });
+}
+
+exports.prototype.firstRun = function firstRun() {
+    var _this = this,
+        _tasks = _this._tasks,
+        firstRunDelay = _this.firstRunDelay;
+
+    if (_tasks.length == 0) {
+        return;
+    }
+
+    var task = _tasks.shift();
+    task._runAt = +new Date();
+    this.dispatch(task, function() {
+        setTimeout(function() {
+            _this.firstRun();
+        }, firstRunDelay);
+    });
+}
+
+exports.prototype.processQueue = function processQueue() {
+    var _this = this,
+        _queue = _this._queue;
+
+    var now = +new Date();
+    if (_queue.length > 0 && _queue[0]._runAt < now) {
+        var task = _queue.shift();
+        _this.dispatch(task);
+    }
+
+    var delay = _this.busyWaitDelay;
+    if (_queue.length > 0) {
+        delay = _queue[0]._runAt - now;
+    }
+
+    setTimeout(function() {
+        _this.processQueue();
+    }, delay);
+}
